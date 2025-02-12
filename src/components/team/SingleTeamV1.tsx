09258988422e08a0a3064de2f74fa3f30ce6d468
MAAONAD <moneyglitchtool@gmail.com>
2025-02-12 20:32:11 +0100

    Update SingleTeamV1.tsx

--- src/components/team/SingleTeamV1.tsx
+++ src/components/team/SingleTeamV1.tsx
@@ -14,21 +14,21 @@ const SingleTeamV1 = ({ member }: { member: DataType }) => {
 
     return (
         <>
-            <div className="team-member-box" data-aos="fade-up" data-aos-delay={delay}>
-                <div className="img-box">
-                    <Link to={`/team-details/${id}`}>
-                        <img src={`/assets/images/${thumb}`} alt="team-img" />
-                    </Link>
-                </div>
-                <div className="content">
-                    <Link to={`/team-details/${id}`}>
-                        <span className="name">{name}</span>
-                    </Link>
-                    <span className="designation">{designation}</span>
-                    <div className="shape"><span /></div>
-                    <p>{text}</p>
-                </div>
-            </div>
+<div className="team-member-box" data-aos="fade-up" data-aos-delay={delay}>
+  <div className="img-box">
+    <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
+      <img src={`/assets/images/${thumb}`} alt="team-img" />
+    </a>
+  </div>
+  <div className="content">
+    <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
+      <span className="name">{name}</span>
+    </a>
+    <span className="designation">{designation}</span>
+    <div className="shape"><span /></div>
+    <p>{text}</p>
+  </div>
+</div>
         </>
     );
 };
